4b5553ad14816079a347a21ca6c14e94c44b96bd
Kevin98Elez <123480475+Kevin98Elez@users.noreply.github.com>
2023-01-26 11:36:20 +0100

    Add files via upload

---
+++ Oefeningen/Oefeningen/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim i As Double = TextBox1.Text
+        Dim kwadraat_i = i ^ 2
+        MsgBox(kwadraat_i & " dit is het kwadraat van " & i)
+    End Sub
+
+End Class
